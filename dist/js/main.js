c86b1a37a818c75fe83697d99db8a37a4d5d4a4b
shivam <thelegendofawizard@gamil.com>
2020-08-07 13:56:19 +0530

    made menu modular

--- dist/js/main.js
+++ dist/js/main.js
@@ -5,6 +5,10 @@ const menuNav = document.querySelector('.menu-nav');
 const menuBranding = document.querySelector('.menu-branding');
 const navItems = document.querySelectorAll('.nav-item');
 
+
+
+
+
 //Set Initial State of the menu
 let showMenu = false;
 const toggleMenu = () => {
